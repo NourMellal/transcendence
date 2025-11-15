8aaf697e5f439a271daabbb9051a99dd6f93f30d
sh1hvb <medchihab651@protonmail.com>
2025-11-15 20:07:43 +0100

    chore(shared-messaging): drop unused event index files

--- packages/shared-messaging/src/events/friendship/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './FriendshipRequestedIntegrationEvent.js';
-export * from './FriendshipAcceptedIntegrationEvent.js';
-export * from './FriendshipRejectedIntegrationEvent.js';
-export * from './FriendshipCancelledIntegrationEvent.js';
-export * from './FriendshipRemovedIntegrationEvent.js';
-export * from './FriendshipBlockedIntegrationEvent.js';
-export * from './FriendshipUnblockedIntegrationEvent.js';
